20c5e3e53b9a5e717a34f9ecdf6d36bec6b437a1
Kost mans <mansurkostoev679@gmail.com>
2023-11-18 09:52:47 +0300

    Класс Tank добавлен

--- MainProject/Tanks/Tank.cs
+++ MainProject/Tanks/Tank.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MainProject.TankAttributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,22 @@ namespace MainProject.Tanks
 {
     public abstract class Tank
     {
-
+        public abstract float Modificator { get; }
+        public abstract Armor Armor { get; protected set; }
+        public abstract Health Health { get; protected set;}
+        public abstract IShell Shell { get; protected set; }
+        public Tank(Armor armor, Health health,IShell shell)
+        {
+            Shell = shell;
+            Armor = armor;
+            Health = health;
+        }
+        public DamageInfo GetDamageInfo()
+        {
+            DamageInfo damage = new DamageInfo();
+            damage.Shell = Shell;
+            damage.Tank = this;
+            return damage;
+        } 
     }
 }
